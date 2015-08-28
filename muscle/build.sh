a6fad5c067f38a3848c0c4c57d8c16439f35c214
Gus Dunn <wadunn83@gmail.com>
2015-08-28 15:23:58 -0400

    added and tested muscle and vsearch recipes

---
+++ muscle/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+install -m 755 -D muscle3.8.31_i86linux64 $PREFIX/bin/muscle
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
---
+++ vsearch/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+install -D vsearch-1.1.3-linux-x86_64 $PREFIX/bin/vsearch
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
