7553c6f0ae934f0ab233f94e0a92454b79381ee1
Gus Dunn <wadunn83@gmail.com>
2015-09-24 16:40:55 -0400

    new build number for vsearch that uses pre-build hopefully static linked binary

--- vsearch/build.sh
+++ vsearch/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -x
 
-cd src/
-make -f Makefile.ZLIB
+wget https://raw.githubusercontent.com/torognes/vsearch/master/LICENSE
 
-install -D vsearch-gz $PREFIX/bin/vsearch
-install -D ../LICENSE $PREFIX/share/doc/vsearch-1.1.3/LICENSE
+install -D vsearch-1.1.3-linux-x86_64 $PREFIX/bin/vsearch
+install -D LICENSE $PREFIX/share/doc/vsearch-1.1.3/LICENSE
 
 # Add more build steps here, if they are necessary.
 
