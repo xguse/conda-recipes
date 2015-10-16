f6c92194c483eac95bc8fb75aaa9d7522795c60d
Gus Dunn <wadunn83@gmail.com>
2015-10-16 14:03:18 -0400

    recipe added for parallel

---
+++ parallel/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./configure --prefix=$PREFIX
+
+make
+
+make install
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
